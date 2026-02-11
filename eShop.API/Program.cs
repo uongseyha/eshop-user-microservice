0a8ba533eb9d08f379bb60d49666d847b3def0d4
Seyha Uong <uong.seyha@gmail.com>
2026-02-11 17:26:00 -0500

    Integrate Swagger

--- eShop.API/Program.cs
+++ eShop.API/Program.cs
@@ -19,6 +19,22 @@ builder.Services.AddControllers().AddJsonOptions(options => {
 //Fluent validation
 builder.Services.AddFluentValidationAutoValidation();
 
+//Add API explorer services
+builder.Services.AddEndpointsApiExplorer();
+
+//Add swagger generation services to create swagger specification
+builder.Services.AddSwaggerGen();
+
+//Add cors services
+builder.Services.AddCors(options =>
+{
+  options.AddDefaultPolicy(builder => {
+    builder.WithOrigins("http://localhost:4200")
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+  });
+});
+
 builder.Services.AddAutoMapper(typeof(ApplicationUserMappingProfile).Assembly);
 
 //Build the web application
@@ -28,6 +44,9 @@ app.UseExceptionHandlingMiddleware();
 
 //Routing
 app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors();
 
 //Auth
 app.UseAuthentication();
