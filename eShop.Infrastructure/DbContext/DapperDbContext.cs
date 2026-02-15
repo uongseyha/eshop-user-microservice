761d9cc4d611e7c65df76b79bce91fdc21671822
Seyha Uong <uong.seyha@gmail.com>
2026-02-15 18:05:53 -0500

    Implement docker compose

--- eShop.Infrastructure/DbContext/DapperDbContext.cs
+++ eShop.Infrastructure/DbContext/DapperDbContext.cs
@@ -12,11 +12,14 @@ public class DapperDbContext
 
     public DapperDbContext(IConfiguration configuration)
     {
-        _configuration = configuration;
-        string? connectionString = _configuration.GetConnectionString("PostgresConnection");
+      _configuration = configuration;
+      string connectionStringTemplate = _configuration.GetConnectionString("PostgresConnection")!;
+      string connectionString = connectionStringTemplate
+      .Replace("$POSTGRES_HOST", Environment.GetEnvironmentVariable("POSTGRES_HOST"))
+      .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
 
-        //Create a new NpgsqlConnection with the retrieved connection string
-        _connection = new NpgsqlConnection(connectionString);
+      //Create a new NpgsqlConnection with the retrieved connection string
+      _connection = new NpgsqlConnection(connectionString);
     }
 
     public IDbConnection DbConnection => _connection;
